2937457ee49db57d8713b7997f93268593a07bb7
Longwei Su <longwei.su@gmail.com>
2021-12-07 00:52:36 -0500

    change to json res

--- routes/users.js
+++ routes/users.js
@@ -9,69 +9,62 @@ const client = new Redis({
   port: 6379,
 });
 
-
-
-function fetchAndRender(searchTerm, res) {
-  try {
-    client.get(searchTerm, async (err, jobs) => {
-      if (err) throw err;
-
-      //TODO render not json
-      if (jobs) {
-        // res.render("account", {title: 'title'});
-        // console.log(Array.isArray(JSON.parse(jobs)))
-        const demo = {
-          postId: searchTerm,
-          id: 1,
-          name: 'from cache',
-          array: JSON.parse(jobs)
-        };
-        res.render('account', demo);
-      }
-      else {
-        console.log("cache miss, pulling new data")
-        const jobs = await axios.get(`https://jsonplaceholder.typicode.com/posts/1/comments`);
-        client.setex(searchTerm, 100000, JSON.stringify(jobs.data));
-
-        // console.log(jobs.data);
-        const demo = {
-          postId: searchTerm,
-          id: 1,
-          name: 'hardpull',
-          array: jobs.data
-        };
-        res.render('account', demo);
-      }
-    });
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-}
-
-
-router.get('/', function (req, res, next) {
-  console.log("??=>" + req.query.title)
-  const searchTerm = req.query.title
-  fetchAndRender(searchTerm, res)
-});
-
-router.get('/:id', function (req, res, next) {
-  console.log(req.params.id)
-  const searchTerm = req.params.id;
-  fetchAndRender(searchTerm, res)
-});
-
-router.post('/:id', function (req, res, next) {
-  console.log(req.params.id)
-  const searchTerm = req.params.id;
+function hardPullAndResponse(searchTerm, res){
+  console.log("hardpull")
   client.del(searchTerm, (error, result)=>{
     if (error) {
       console.log(error)
       return;
     }
     console.log(result);
-    fetchAndRender(searchTerm, res);
+    softPullAndResponse(searchTerm, res);
   });
+}
+
+function softPullAndResponse(searchTerm, res) {
+  try {
+    client.get(searchTerm, async (err, jobs) => {
+      if (err) throw err;
+      if (jobs) {
+        console.log("from cache")
+        res.status(200).send(jobs);
+      }
+      else {
+        const jobs = await axios.get(`https://jsonplaceholder.typicode.com/posts/1/comments`);
+        client.setex(searchTerm, 100000, JSON.stringify(jobs.data));
+        console.log("hard pull")
+        res.status(200).send(jobs.data);
+      }
+    });
+  } catch (err) {
+    res.status(500).send({ message: "invalid handler" });
+  }
+}
+
+
+router.get('/', function (req, res, next) {
+  console.log("get /user" + req.query.title)
+  const searchTerm = req.query.title
+  softPullAndResponse(searchTerm, res)
+});
+
+router.get('/:id', function (req, res, next) {
+  console.log(req.params.id)
+  const searchTerm = req.params.id;
+  softPullAndResponse(searchTerm, res)
+});
+
+router.get('/:id/:now', function (req, res, next) {
+  console.log(req.params.id)
+  console.log(req.params.now)
+  const searchTerm = req.params.id;
+  hardPullAndResponse(searchTerm, res)
+});
+
+router.post('/:id', function (req, res, next) {
+  console.log(req.params.id)
+  const searchTerm = req.params.id;
+  hardPullAndResponse(searchTerm, res)
 });
 
 module.exports = router;
